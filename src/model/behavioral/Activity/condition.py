0d0b8bcad8452af92839ecc9f9f98711d6c0822b
Abe Mitsuteru <rithgroove@gmail.com>
2022-02-01 19:22:51 +0900

    fix the condition with operator

--- src/model/behavioral/Activity/condition.py
+++ src/model/behavioral/Activity/condition.py
@@ -1,10 +1,23 @@
+import operator
 class Condition:
-
-	def __init__(self,name,value, ""):
-		name = n
+	def __init__(self,name,value, operator_string):
+		name = name
+		value = value
+		self.operator = _fetch_operator(operator_string)
 
 	def check_value(self,agent)
-		if agent.get_attribute(self.name) < self.value
-			return True
-		else 
-			return False
+		return self.operator(agent.get_attribute(self.name), value)
+
+def _fetch_operator(operator_string):
+	if(operator_string.lower() == "greater_than_equal")
+		return operator.ge
+	elif(operator_string.lower() == "greater_than")
+		return operator_string.gt
+	elif(operator_string == "less_than_equal")
+		return operator_string.le
+	elif(operator_string == "less_than")
+		return operator_string.lt
+	elif(operator_string == "equal")
+		return operator_string.eq
+	elif(operator_string == "not_equal")
+		return operator_string.ne
