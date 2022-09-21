3716242213322b778f86a0df4f7227afeeb0932a
k1 <fhktanaka@gmail.com>
2022-09-21 11:47:50 +0900

    window res functionn moved to utils

--- src/view/view.py
+++ src/view/view.py
@@ -4,6 +4,7 @@ import tkinter.filedialog as tk_fdialog
 import numpy as np
 
 from src.view.draw import Draw
+import src.view.util as util
 
 class View():
     def __init__(self, window_size=(1024, 768)):
@@ -15,14 +16,17 @@ class View():
         bg_btn,font_btn,bg_log,font_log ="white","sans 10 bold","white","sans 10 bold"
         ### ###
 
-        self.window_size = self.get_window_resolution(window_size[0], window_size[1])
 
         # root
         self.root = tk.Tk()
+        geometry_str = util.make_geometry_string(self.root, window_size)
+
         #self.root.configure(background="black")
         self.root.title("Community Simulator")
-        self.root.geometry(self.make_geometry_string(window_size))
+        self.root.geometry(geometry_str)
         self.root.resizable(False, False)
+
+        self.window_size = util.get_window_resolution(self.root, window_size[0], window_size[1])
 
         ## main bar and notebooks
         self.frame_bar = ttk.Frame(self.root)
@@ -82,6 +86,7 @@ class View():
     ## main methods ##
     def main_loop(self):
         self.root.mainloop()
+
     def close(self):
         self.root.destroy()
 
@@ -94,12 +99,6 @@ class View():
     def zoom(self, scale):
         self.canvas.scale('all', 0, 0, scale, scale)
 
-
-
-
-    def update_view(self):
-        pass
-
     def btn_start_change_method(self, text, method):
         self.buttons["sim_run"]["text"]    = text
         self.buttons["sim_run"]["command"] = method
@@ -108,46 +107,6 @@ class View():
         self.buttons["sim_step"]["state"] = tk.NORMAL
         if text == "Pause": # auto-running
             self.buttons["sim_step"]["state"] = tk.DISABLED
-
-    ## log ##
-    def update_log(self, txt):
-        print(txt)
-        #todo: widget or tab where we print messages
-
-    ## screen positioning methods ##
-    def get_window_resolution(self, window_width, window_height):
-        width = 1024
-        height = 768
-
-        # If a width contains a "%", the window size is relative to the screen size
-        # if it contains only a int, its an absolute value
-
-        if isinstance(window_width, str) and window_width[-1] == "%":
-            scale = int(window_width[:-1])/100
-            width = self.root.winfo_screenwidth() * scale
-        elif isinstance(window_width, int):
-            width = window_width
-
-        if isinstance(window_height, str) and window_height[-1] == "%":
-            scale = int(window_height[:-1])/100
-            height = self.root.winfo_screenheight() * scale
-        elif isinstance(window_height, int):
-            height = window_height
-
-        return (width, height)
-    def get_center_screen(self, window_size):
-        # get the screen dimension
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-
-        # find the center point
-        center_x = int(screen_width/2 - window_size[0]/2)
-        center_y = int(screen_height/2 - window_size[1]/2)
-
-        return center_x, center_y
-    def make_geometry_string(self, window_size):
-        self.center_x, self.center_y = self.get_center_screen(window_size)
-        return f"{window_size[0]}x{window_size[1]}+{self.center_x}+{self.center_y}"
 
 if __name__ == "__main__":
     view = View()
